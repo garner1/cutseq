8ead0a0615d8bdcc0ca42b4b1d188e89ac525c88
garner1 <silvano.garnerone@gmail.com>
2016-08-15 11:03:47 +0200

    - ‘bin/module/.#compare_DSB_segmentation.sh’ / ‘bin/module/compare_DSB_segmentation.sh’

--- bin/module/compare_DSB_segmentation.sh
+++ bin/module/compare_DSB_segmentation.sh
@@ -4,6 +4,7 @@ dsb1=$1			# this is the full path to the DSB data bed file *UMI_count*
 dsb2=$2			# this is the full path to the DSB data bed file *UMI_count*
 quality=$3		# filter out low quality reads
 resolution=$4		# resolution of the binned genome
+metric=$4			# can be {asym,norm_1,norm_2}
 
 /home/garner1/SparkleShare/RESTSEQ/bin/module/DSB_segmentation.sh $dsb1 $quality $resolution & pid1=$!
 /home/garner1/SparkleShare/RESTSEQ/bin/module/DSB_segmentation.sh $dsb2 $quality $resolution & pid2=$!
@@ -13,7 +14,8 @@ wait $pid2
 temp_file=$(mktemp)
 bedtools unionbedg -i "$dsb1"__normed_segmented_q"$quality"_res"$resolution".bed "$dsb2"__normed_segmented_q"$quality"_res"$resolution".bed -header -names PROB1 PROB2 > ${temp_file}
 
-head ${temp_file}
+python ~/Work/pipelines/restseq/bin/module/KL_divergence.py ~/Work/dataset/restseq/p ~/Work/dataset/restseq/"$name" $metric
+
 rm -f ${temp_file}
 
 

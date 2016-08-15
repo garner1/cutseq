2cc091d76cbe44304e786759bcd0048a3a3a1a3a
garner1 <silvano.garnerone@gmail.com>
2016-08-15 09:30:16 +0200

    - ‘bin/module/.#segmentation.sh’ / ‘bin/module/segmentation.sh’

--- bin/module/segmentation.sh
+++ bin/module/segmentation.sh
@@ -4,7 +4,7 @@ dsb=$1			# this is the full path to the DSB data bed file *UMI_count*
 resolution=$2			# resolution of the binned genome
 
 #create bed files representing the binned genome
-~/Work/pipelines/aux.scripts/make-windows.sh $resolution hg19 > "$dsb"__A.bed
+~/SparkleShare/aux.scripts/make-windows.sh $resolution hg19 > "$dsb"__A.bed
 
 #############################################################
 #count the number of molecules in each bin

b777f3adb1c1ee17217b7cf229ae46e2ecef4c39
garner1 <silvano.garnerone@gmail.com>
2016-08-15 11:57:32 +0200

    - ‘bin/module/.#DSB_segmentation.sh’ < ‘bin/module/#DSB_segmentation.sh#’ > ‘bin/module/DSB_segmentation.sh’

--- bin/module/DSB_segmentation.sh
+++ bin/module/DSB_segmentation.sh
@@ -12,9 +12,9 @@ resolution=$3		# resolution of the binned genome
 cat "$dsb"| awk -v quality="$quality" '$6 >= quality' |
 bedtools intersect -a "$dsb"__A.bed -b stdin -c | grep -v "chrM" > "$dsb"__raw_segmented_q"$quality"_res"$resolution".bed 
 
-norma=$(cat "$dsb"__raw_segmented_q"$quality"_res"$resolution".bed | datamash sum 4)
-cat "$dsb"__raw_segmented_q"$quality"_res"$resolution".bed | 
-awk -v norma="$norma" '{FS=OFS="\t"; print $1,$2,$3,$4/norma}' > "$dsb"__normed_segmented_q"$quality"_res"$resolution".bed
+# norma=$(cat "$dsb"__raw_segmented_q"$quality"_res"$resolution".bed | datamash sum 4)
+# cat "$dsb"__raw_segmented_q"$quality"_res"$resolution".bed | 
+# awk -v norma="$norma" '{FS=OFS="\t"; print $1,$2,$3,$4/norma}' > "$dsb"__normed_segmented_q"$quality"_res"$resolution".bed
 
 # rm -f "$dsb"__raw_segmented_q"$quality"_res"$resolution".bed
 

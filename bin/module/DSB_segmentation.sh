d0467283c7a6e3c8c4e7587f4d2659cd38b5b0c4
garner1 <silvano.garnerone@gmail.com>
2016-08-15 15:27:27 +0200

    - ‘bin/module/.#DSB_segmentation.sh’ / ‘bin/module/DSB_segmentation.sh’

--- bin/module/DSB_segmentation.sh
+++ bin/module/DSB_segmentation.sh
@@ -5,12 +5,13 @@ quality=$2		# filter out low quality reads
 resolution=$3		# resolution of the binned genome
 
 #create bed files representing the binned genome
-~/SparkleShare/aux.scripts/make-windows.sh $resolution hg19 > "$dsb"__A.bed
+temp_file_0=$(mktemp)
+~/SparkleShare/aux.scripts/make-windows.sh $resolution hg19 > ${temp_file_0}
 
 #############################################################
 #count the number of molecules in each bin
 cat "$dsb"| awk -v quality="$quality" '$6 >= quality' |
-bedtools intersect -a "$dsb"__A.bed -b stdin -c | grep -v "chrM" > "$dsb"__raw_segmented_q"$quality"_res"$resolution".bed 
+bedtools intersect -a ${temp_file_0} -b stdin -c | grep -v "chrM" > "$dsb"__raw_segmented_q"$quality"_res"$resolution".bed 
 
 # norma=$(cat "$dsb"__raw_segmented_q"$quality"_res"$resolution".bed | datamash sum 4)
 # cat "$dsb"__raw_segmented_q"$quality"_res"$resolution".bed | 

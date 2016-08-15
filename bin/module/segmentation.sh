c5e3f8b3b004a48cfa59676e65ac126c5b0c8279
garner1 <silvano.garnerone@gmail.com>
2016-08-15 09:39:51 +0200

    - ‘bin/module/.#segmentation.sh’ / ‘bin/module/segmentation.sh’

--- bin/module/segmentation.sh
+++ bin/module/segmentation.sh
@@ -9,4 +9,6 @@ resolution=$2			# resolution of the binned genome
 #############################################################
 #count the number of molecules in each bin
 bedtools intersect -a "$dsb"__A.bed -b "$dsb" -c > "$dsb"__raw_segmented.bed 
+norma=$(cat "$dsb"__raw_segmented.bed | datamash sum 4)
+echo $norma
 

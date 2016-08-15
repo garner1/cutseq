82e148da6b98b1cf57b501e1e8b0aa6001cd9a36
garner1 <silvano.garnerone@gmail.com>
2016-08-15 11:09:08 +0200

    - ‘bin/module/.#compare_DSB_segmentation.sh’ / ‘bin/module/compare_DSB_segmentation.sh’

--- bin/module/compare_DSB_segmentation.sh
+++ bin/module/compare_DSB_segmentation.sh
@@ -6,8 +6,8 @@ quality=$3		# filter out low quality reads
 resolution=$4		# resolution of the binned genome
 metric=$4			# can be {asym,norm_1,norm_2}
 
-/home/garner1/SparkleShare/RESTSEQ/bin/module/DSB_segmentation.sh $dsb1 $quality $resolution & pid1=$!
-/home/garner1/SparkleShare/RESTSEQ/bin/module/DSB_segmentation.sh $dsb2 $quality $resolution & pid2=$!
+~/SparkleShare/RESTSEQ/bin/module/DSB_segmentation.sh $dsb1 $quality $resolution & pid1=$!
+~/SparkleShare/RESTSEQ/bin/module/DSB_segmentation.sh $dsb2 $quality $resolution & pid2=$!
 wait $pid1
 wait $pid2
 
@@ -20,7 +20,7 @@ tail -n +2 ${temp_file_1} | cut -f4 > ${temp_file_2}
 temp_file_3=$(mktemp)
 tail -n +2 ${temp_file_1} | cut -f4 > ${temp_file_3}
 
-python ~/Work/pipelines/restseq/bin/module/KL_divergence.py ${temp_file_2} ${temp_file_3} "$metric"
+python ~/SparkleShare/RESTSEQ/bin/module/KL_divergence.py ${temp_file_2} ${temp_file_3} "$metric"
 
 rm -f ${temp_file_1} ${temp_file_2} ${temp_file_3}
 

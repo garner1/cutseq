f89f5943bd0cdc6fb855a5e12815d6785fb98900
garner1 <silvano.garnerone@gmail.com>
2016-08-15 11:07:52 +0200

    - ‘bin/module/.#compare_DSB_segmentation.sh’ < ‘bin/module/#compare_DSB_segmentation.sh#’ > ‘bin/module/compare_DSB_segmentation.sh’

--- bin/module/compare_DSB_segmentation.sh
+++ bin/module/compare_DSB_segmentation.sh
@@ -11,12 +11,18 @@ metric=$4			# can be {asym,norm_1,norm_2}
 wait $pid1
 wait $pid2
 
-temp_file=$(mktemp)
-bedtools unionbedg -i "$dsb1"__normed_segmented_q"$quality"_res"$resolution".bed "$dsb2"__normed_segmented_q"$quality"_res"$resolution".bed -header -names PROB1 PROB2 > ${temp_file}
+temp_file_1=$(mktemp)
+bedtools unionbedg -i "$dsb1"__normed_segmented_q"$quality"_res"$resolution".bed "$dsb2"__normed_segmented_q"$quality"_res"$resolution".bed -header -names PROB1 PROB2 > ${temp_file_1}
 
-python ~/Work/pipelines/restseq/bin/module/KL_divergence.py ~/Work/dataset/restseq/p ~/Work/dataset/restseq/"$name" $metric
+temp_file_2=$(mktemp)
+tail -n +2 ${temp_file_1} | cut -f4 > ${temp_file_2}
 
-rm -f ${temp_file}
+temp_file_3=$(mktemp)
+tail -n +2 ${temp_file_1} | cut -f4 > ${temp_file_3}
+
+python ~/Work/pipelines/restseq/bin/module/KL_divergence.py ${temp_file_2} ${temp_file_3} "$metric"
+
+rm -f ${temp_file_1} ${temp_file_2} ${temp_file_3}
 
 
 

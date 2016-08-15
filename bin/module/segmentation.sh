e1d52aaa46c3932e58b4e8291bbdb5cedc834f98
garner1 <silvano.garnerone@gmail.com>
2016-08-15 09:53:57 +0200

    - ‘bin/module/.#segmentation.sh’ / ‘bin/module/segmentation.sh’

--- bin/module/segmentation.sh
+++ bin/module/segmentation.sh
@@ -11,7 +11,11 @@ resolution=$3		# resolution of the binned genome
 #count the number of molecules in each bin
 cat "$dsb"| awk -v quality="$quality" '$6 >= quality' |
 bedtools intersect -a "$dsb"__A.bed -b stdin -c | grep -v "chrM" > "$dsb"__raw_segmented_q"$quality"_res"$resolution".bed 
+
 norma=$(cat "$dsb"__raw_segmented_q"$quality"_res"$resolution".bed | datamash sum 4)
+cat "$dsb"__raw_segmented_q"$quality"_res"$resolution".bed | awk -v norma="$norma" '{FS=OFS="\t"; print $1,$2,$3,$4/norma}'|head
+
 echo $norma
+
 
 

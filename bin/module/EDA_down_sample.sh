afb6bc383b0f2bdd79ee3304a4f348b3e45828aa
garner1 <silvano.garnerone@gmail.com>
2016-08-18 14:55:16 +0200

    - ‘bin/module/#EDA_down_sample.sh#’ - ‘bin/module/.#EDA_down_sample.sh’ / ‘bin/module/EDA_down_sample.sh’

--- bin/module/EDA_down_sample.sh
+++ bin/module/EDA_down_sample.sh
@@ -3,7 +3,10 @@
 in=$1				# input bed file
 size=$2				# size of the random sample
 
-for ind in $(seq 2);do
-    out="$ind"__$(echo "$in"|tr '/' '_')
-    cat $in | awk '$6>=10' | shuf -n $size -o $out -
-done
+# for ind in $(seq 2);do
+#     out="$ind"__$(echo "$in"|tr '/' '_')
+#     cat $in | awk '$6>=10' | shuf -n $size -o $out -
+# done
+
+
+parallel "temp_{}=$(mktemp);cat $temp_{} | awk '\$6>=10' | shuf -n $size -o {}__$(echo $in|tr '/' '_') -"  ::: $(seq 2)

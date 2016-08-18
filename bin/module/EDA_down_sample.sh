8c738b2992327fcea99eef561047425a3369af2e
garner1 <silvano.garnerone@gmail.com>
2016-08-18 14:48:59 +0200

    - ‘bin/module/.#EDA_down_sample.sh’ / ‘bin/module/EDA_down_sample.sh’

--- bin/module/EDA_down_sample.sh
+++ bin/module/EDA_down_sample.sh
@@ -4,6 +4,6 @@ in=$1				# input bed file
 size=$2				# size of the random sample
 
 for ind in $(seq 2);do
-    out=$"ind"__$(echo $in|tr '/' '_')
+    out="$ind"__$(echo "$in"|tr '/' '_')
     shuf -n $size -o $out $in
 done

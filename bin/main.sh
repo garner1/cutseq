dc74ecb9af6bc7d8699faa8b7fe7a4764b161d11
garner1 <silvano.garnerone@gmail.com>
2017-12-12 17:47:47 +0100

    major recoding

--- bin/main.sh
+++ bin/main.sh
@@ -8,20 +8,18 @@
 # DEFINING VARIABLES
 experiment=$1	     # xz13-14-15-10PE
 genome=$2	     # hg19
-quality=$3	     # at least 3 to get rid of secondary alignments in repeats
-resolution=$4	     # 10000000
-mode=$5		     # PE or SE
-barcode_file=$6	     # ~/Work/pipelines/restseq/pattern/barcode-cutsite_18
-cutsite=$7	     # the restriction cutsite
-umi_missmatch=$8     # threshold on the UMI mismatch for filtering
-r1=$9		     # ~/Work/dataset/XZ14_ACAGTG_L001_R1_001.fastq.gz
-r2=${10}	     # ~/Work/dataset/XZ14_ACAGTG_L001_R2_001.fastq.gz
+mode=$3		     # PE or SE
+barcode_file=$4	     # ~/Work/pipelines/restseq/pattern/barcode-cutsite_18
+cutsite=$5	     # the restriction cutsite
+umi_missmatch=$6     # threshold on the UMI mismatch for filtering
+r1=$7		     # ~/Work/dataset/XZ14_ACAGTG_L001_R1_001.fastq.gz
+r2=$8	     # ~/Work/dataset/XZ14_ACAGTG_L001_R2_001.fastq.gz
 numbproc=32
 
 ################################################################################
 
 # PREPARE DIRECTORY STRUCTURE
-datadir=$HOME/Work/dataset/restseq && mkdir -p $datadir/$experiment
+datadir=$HOME/Work/dataset/reduced_sequencing && mkdir -p $datadir/$experiment
 in="$datadir"/"$experiment"/indata && mkdir -p $in
 out="$datadir"/"$experiment"/outdata && mkdir -p $out
 aux="$datadir"/"$experiment"/auxdata && mkdir -p $aux
@@ -31,12 +29,12 @@ echo
 echo Processing $experiment
 ################################################################################
 
-~/Work/pipelines/restseq/binDev/module/quality_control.sh $r1 $numbproc $out
+# ~/Work/pipelines/restseq/binDev/module/quality_control.sh $r1 $numbproc $out
 
 ################################################################################
 
 # USE EITHER prepare_files.sh OR parallel_scan.sh (in case of multiple barcodes)
-~/Work/pipelines/restseq/binDev/module/parallel_scan.sh $in $mode $barcode_file $r1 $r2
+./module/parallel_scan.sh $cutsite $in $mode $barcode_file $r1 $r2 
 
 ################################################################################
 
@@ -67,33 +65,33 @@ do
 	then
 	    samtools view -h -Sb "$out"/"$barcode".sam > "$out"/"$barcode".bam # only keep first mate in pair
 	fi
-    	$PWD/module/umi_joining.sh $in $aux $barcode "$out"/"$barcode".bam "$out"/q"$quality"_withUMI.bam
+    	$PWD/module/umi_joining.sh $in $aux $barcode "$out"/"$barcode".bam "$out"/withUMI.bam
 
     	echo "Filter UMIs ..."
-	bedtools bamtobed -i "$out"/q"$quality"_withUMI.bam | sort --parallel=8 --temporary-directory=$HOME/tmp -k1,1 -k2,2n > "$aux"/myfile_"$barcode" # convert bam2bed
+	bedtools bamtobed -i "$out"/withUMI.bam | sort --parallel=8 --temporary-directory=$HOME/tmp -k1,1 -k2,2n > "$aux"/myfile_"$barcode" # convert bam2bed
 	if [ -s "$aux"/myfile_"$barcode" ]; then # check if file is not empty
 	    bedtools closest -a "$aux"/myfile_"$barcode" -b ~/Work/pipelines/data/"$cutsite".bed -d | 
 	    LC_ALL=C sort --parallel=8 --temporary-directory=$HOME/tmp -k4,4 | sed 's/\/1//' > "$aux"/output_"$barcode" # find the closest cutsite 
 	    	    
-	    samtools view "$out"/chr-loc-umi_q"$quality".bam | LC_ALL=C sort --parallel=8 --temporary-directory=$HOME/tmp -k1,1 | # select first mate of pairs
+	    samtools view "$out"/withUMI.bam | LC_ALL=C sort --parallel=8 --temporary-directory=$HOME/tmp -k1,1 | # select first mate of pairs
 	    LC_ALL=C join -1 1 -2 4 - "$aux"/output_"$barcode"| # join WRT to read ID
-	    tr " " "\t" | awk '{FS=OFS="\t";print $(NF-3),$(NF-2),$(NF-1),$NF,$(NF-4),$(NF-5),$(NF-9),$1,$3,$4}' > "$out"/cutsite_dist_strand_qScore_UMI_ID_start__"$barcode"_q"$quality".bed
+	    tr " " "\t" | awk '{FS=OFS="\t";print $(NF-8),$(NF-7)+1,$(NF-6)+1,$(NF-4),$(NF-5),$(NF-9),$1,$(NF-3),$(NF-2),$(NF-1),$NF}' > "$out"/read_strand_qScore_UMI_ID_cutsite_dist__"$barcode".bed
 	    
-	    cut -f-7 "$out"/cutsite_dist_strand_qScore_UMI_ID_start__"$barcode"_q"$quality".bed | 
-	    datamash -s -g 1,2,3,4,5,6,7 count 1,2,3,4,5,6,7 > "$out"/cutsite_dist_strand_qScore_UMI_count__"$barcode"_q"$quality".bed
+	    cut -f-7 "$out"/read_strand_qScore_UMI_ID_cutsite_dist__"$barcode".bed | 
+	    datamash -s -g 1,2,3,4,5,6 count 1,2,3,4,5,6 | cut -f-7 > "$out"/read_strand_qScore_UMI_count__"$barcode".bed
 
 	    rm -f chr*
-	    cat "$out"/cutsite_dist_strand_qScore_UMI_count__"$barcode"_q"$quality".bed | grep -v "^\." |
+	    cat "$out"/read_strand_qScore_UMI_count__"$barcode".bed | grep -v "^\." |
 	    awk '{print >> $1; close($1)}' -  # split file according to chromosome
 
-    	    chr_list=$(cut -f1 "$out"/cutsite_dist_strand_qScore_UMI_count__"$barcode"_q"$quality".bed|grep -v "_\|^\."|LC_ALL=C sort -u)
+    	    chr_list=$(cut -f1 "$out"/read_strand_qScore_UMI_count__"$barcode".bed|grep -v "_\|^\."|LC_ALL=C sort -u)
 	    parallel "python $PWD/module/umi_filtering.py $PWD/{} $umi_missmatch {} {}_out.bed" ::: $(echo $chr_list)
     	    cat chr*_out.bed | tr -d "," | tr -d "'" | tr -d "[" | tr -d "]" | tr " " "\t" | grep -v "_" | 
-	    LC_ALL=C sort --parallel=8 --temporary-directory=$HOME/tmp -k1.4n -k2,2n > "$out"/cutsite_dist_strand_qScore_UMI_count__"$barcode"_q"$quality".bed
+	    LC_ALL=C sort --parallel=8 --temporary-directory=$HOME/tmp -k1.4n -k2,2n > "$out"/read_strand_qScore_UMI_count__"$barcode".bed
 	    rm -f chr*
     	fi
     fi
 done
 
-rm -fr "$in"/barcode_* "$out"/*.{sam,bam} "$aux"/* 	# !!!clean outdata and auxdata directories!!!!
+# rm -fr "$in"/barcode_* "$out"/*.{sam,bam} "$aux"/* 	# !!!clean outdata and auxdata directories!!!!
  

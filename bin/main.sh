46f685691fdb4c5318a38f32945a094f3026f646
garner1 <silvano.garnerone@gmail.com>
2018-01-10 15:33:31 +0100

    general

--- bin/main.sh
+++ bin/main.sh
@@ -65,12 +65,12 @@ do
     	$PWD/module/umi_joining.sh $in $aux $barcode "$out"/"$barcode".bam "$out"/withUMI.bam
 
     	echo "Filter UMIs ..."
-	bedtools bamtobed -i "$out"/withUMI.bam | sort --parallel=8 --temporary-directory=$HOME/tmp -k1,1 -k2,2n > "$aux"/myfile_"$barcode" # convert bam2bed
+	bedtools bamtobed -i "$out"/withUMI.bam | sort --parallel=8 --temporary-directory=$HOME/tmp -k1,1 -k2,2n > "$aux"/myfile_"$barcode" # convert bam2bed sorted wrt to chr and start
 	if [ -s "$aux"/myfile_"$barcode" ]; then # check if file is not empty
 	    bedtools closest -a "$aux"/myfile_"$barcode" -b ~/Work/pipelines/data/"$cutsite".bed -d | 
 	    LC_ALL=C sort --parallel=8 --temporary-directory=$HOME/tmp -k4,4 | sed 's/\/1//' > "$aux"/output_"$barcode" # find the closest cutsite 
 	    	    
-	    samtools view "$out"/withUMI.bam | LC_ALL=C sort --parallel=8 --temporary-directory=$HOME/tmp -k1,1 | #!!!THIS SEEMS TO BE A REPETITION WRT LINE 68!! select first mate of pairs
+	    samtools view "$out"/withUMI.bam | LC_ALL=C sort --parallel=8 --temporary-directory=$HOME/tmp -k1,1 | # select first mate of pairs
 	    LC_ALL=C join -1 1 -2 4 - "$aux"/output_"$barcode"| # join WRT to read ID
 	    tr " " "\t" | awk '{FS=OFS="\t";print $(NF-8),$(NF-7)+1,$(NF-6)+1,$(NF-4),$(NF-5),$(NF-9),$1,$(NF-3),$(NF-2),$(NF-1),$NF}' > "$out"/read_strand_qScore_UMI_ID_cutsite_dist__"$barcode".bed
 	    
@@ -95,7 +95,5 @@ do
     fi
 done
 
-
-
-rm -fr "$in"/barcode_* "$out"/*.{sam,bam} "$aux"/* 	# !!!clean outdata and auxdata directories!!!!
+# rm -fr "$in"/barcode_* "$out"/*.{sam,bam} "$aux"/* 	# !!!clean outdata and auxdata directories!!!!
  

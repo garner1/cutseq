c4bbd823a5371eb88fd99f36efb6dcd5792513d3
garner1 <silvano.garnerone@gmail.com>
2018-04-23 10:55:44 +0200

    test.sh deleted

--- bin/test.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-
-EXP=XZ68
-genome=human
-mode=SE
-cutsite=AAGCTT
-fastq=/home/garner1/Work/dataset/reduced_sequencing/test.fq.gz
-time bash main.sh $EXP $genome $mode ../pattern/barcode-cutsite_"$EXP" $cutsite $fastq

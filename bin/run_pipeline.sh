a18152305fef3a16a615e617fc3e1992410b8854
garner1 <silvano.garnerone@gmail.com>
2018-04-26 09:48:34 +0200

    minor

--- bin/run_pipeline.sh
+++ bin/run_pipeline.sh
@@ -1,20 +1,4 @@
 #!/bin/usr/env bash
 
-bash main.sh XZ91 human SE ../pattern/barcode-cutsite_XZ91 CATG /home/garner1/Work/dataset/reduced_sequencing/XZ91.fastq.gz
-
-bash main.sh XZ114 human SE ../pattern/barcode-cutsite_XZ114 CATG /home/garner1/Work/dataset/reduced_sequencing/XZ114.fastq.gz
-
-bash main.sh XZ115 human SE ../pattern/barcode-cutsite_XZ115 CATG /home/garner1/Work/dataset/reduced_sequencing/XZ115.fastq.gz
-
-bash main.sh XZ117 human SE ../pattern/barcode-cutsite_XZ117 CATG /home/garner1/Work/dataset/reduced_sequencing/XZ117.fastq.gz
-
-bash main.sh XZ118 human SE ../pattern/barcode-cutsite_XZ118 CATG /home/garner1/Work/dataset/reduced_sequencing/XZ118.fastq.gz
-
-bash main.sh XZ119 human SE ../pattern/barcode-cutsite_XZ119 CATG /home/garner1/Work/dataset/reduced_sequencing/XZ119.fastq.gz
-
-bash main.sh XZ120 human SE ../pattern/barcode-cutsite_XZ120 CATG /home/garner1/Work/dataset/reduced_sequencing/XZ120.fastq.gz
-
-bash main.sh XZ121 human SE ../pattern/barcode-cutsite_XZ121 CATG /home/garner1/Work/dataset/reduced_sequencing/XZ121.fastq.gz
-
-bash main.sh XZ122 human SE ../pattern/barcode-cutsite_XZ122 CATG /home/garner1/Work/dataset/reduced_sequencing/XZ122.fastq.gz
+bash main.sh MS10 human SE /home/garner1/Dropbox/pipelines/reduced_sequencing/pattern/barcode-cutsite_MS10 CATG /media/bicroserver_2-seq/BICRO82/FASTQ/MS10/MS10.fastq.gz
 

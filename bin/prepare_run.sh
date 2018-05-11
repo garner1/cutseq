f3044e12a9e2ed6ac42c08c0c9b1d8527c94721e
garner1 <silvano.garnerone@gmail.com>
2018-05-11 16:20:51 +0200

    minor changes to prepare patterns and run

--- bin/prepare_run.sh
+++ bin/prepare_run.sh
@@ -5,11 +5,12 @@
 
 inputfile=$1
 dir=$2	     # /media/bicroserver_2-seq/BICRO67/FASTQ
+run=$3
 
-rm -f run_pipeline.sh
+rm -f run_pipeline_"$run".sh
 
-echo "#!/bin/usr/env bash" >> run_pipeline.sh # first line
-echo >> run_pipeline.sh			   # empty space
+echo "#!/bin/usr/env bash" >> run_pipeline_"$run".sh # first line
+echo >> run_pipeline_"$run".sh			   # empty space
 
 while read -r line; do
     lib=`echo $line|cut -d',' -f1`
@@ -19,8 +20,8 @@ while read -r line; do
     cutsite=$cutsite
     echo $barcode$cutsite > ../pattern/barcode-cutsite_$lib
     fastq=$dir/$lib.fastq.gz
-    echo bash main.sh $lib human $mode /home/garner1/Dropbox/pipelines/reduced_sequencing/pattern/barcode-cutsite_$lib $cutsite $fastq >> run_pipeline.sh
-    echo >> run_pipeline.sh
+    echo bash main.sh $lib human $mode /home/garner1/Dropbox/pipelines/reduced_sequencing/pattern/barcode-cutsite_$lib $cutsite $fastq >> run_pipeline_"$run".sh
+    echo >> run_pipeline_"$run".sh
 done < $inputfile
 
-chmod +x run_pipeline.sh
+chmod +x run_pipeline_"$run".sh

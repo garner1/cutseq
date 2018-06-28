c5a1a8b540448c0414afd1ba9bec39bab89ce02b
garner1 <silvano.garnerone@gmail.com>
2018-06-28 15:42:03 +0200

    Paired ended option added

--- bin/main.sh
+++ bin/main.sh
@@ -23,9 +23,11 @@ refgen=~/Work/genomes/Homo_sapiens.GRCh37.dna.primary_assembly.fa/GRCh37.fa # fu
 
 echo
 echo Processing $experiment
+
 ################################################################################
 # bash ./module/quality_control.sh $r1 $numbproc $out
 ################################################################################
+
 bash ./module/parallel_scan.sh $cutsite $in $mode $barcode_file $r1 $r2 
 rm -fr "$out"/* "$aux"/* 	# !!!clean outdata and auxdata directories!!!!
 
@@ -39,6 +41,7 @@ do
     then
     	bwa mem -v 1 -t $numbproc $refgen "$in"/barcode_"$barcode".fq "$in"/barcode_"$barcode"-r2.fq > "$aux"/"$barcode".sam
     fi
+    
     if [ "$mode" == "SE" ]
     then
     	bwa mem -v 1 -t $numbproc $refgen "$in"/barcode_"$barcode".fq > "$aux"/"$barcode".sam
@@ -46,14 +49,20 @@ do
 
     count=$(samtools view -S "$aux"/"$barcode".sam | head -1 | wc -l)
     if [ $count -ne 0 ]; then 
-	if [ "$mode" == "SE" ];	then
-	    samtools view -h -Sb -q $quality "$aux"/"$barcode".sam > "$aux"/"$barcode".bam # only keep first mate in pair and filter wrt quality
-	    samtools sort "$aux"/"$barcode".bam -o "$aux"/"$barcode".sorted.bam
-	    samtools index "$aux"/"$barcode".sorted.bam
-	fi
+    	if [ "$mode" == "SE" ];	then
+    	    samtools view -h -Sb -q $quality "$aux"/"$barcode".sam > "$aux"/"$barcode".bam # only keep first mate in pair and filter wrt quality
+    	    samtools sort "$aux"/"$barcode".bam -o "$aux"/"$barcode".sorted.bam
+    	    samtools index "$aux"/"$barcode".sorted.bam
+    	    umi_tools dedup -I "$aux"/"$barcode".sorted.bam -S "$out"/"$barcode".deduplicated.bam --edit-distance-threshold 2 -L "$out"/"$barcode".group.log # first dedup of reads not at cutsite
+    	fi
+    	if [ "$mode" == "PE" ];	then
+    	    samtools view -h -Sb -q $quality "$aux"/"$barcode".sam > "$aux"/"$barcode".bam # only keep first mate in pair and filter wrt quality
+    	    samtools sort "$aux"/"$barcode".bam -o "$aux"/"$barcode".sorted.bam
+    	    samtools index "$aux"/"$barcode".sorted.bam
+	    umi_tools dedup -I "$aux"/"$barcode".sorted.bam --paired -S "$out"/"$barcode".deduplicated.bam --edit-distance-threshold 2 -L "$out"/"$barcode".group.log 
+    	fi
 
     	echo "Filter UMIs ..."
-	umi_tools dedup -I "$aux"/"$barcode".sorted.bam -S "$out"/"$barcode".deduplicated.bam --edit-distance-threshold 2 -L "$out"/"$barcode".group.log # first dedup of reads not at cutsite
-	bam2bed < "$out"/"$barcode".deduplicated.bam | cut -f-17 > "$out"/"$barcode".deduplicated.bed # convert using bedops bam2bed
+    	bam2bed < "$out"/"$barcode".deduplicated.bam | cut -f-17 > "$out"/"$barcode".deduplicated.bed # convert using bedops bam2bed
     fi
 done

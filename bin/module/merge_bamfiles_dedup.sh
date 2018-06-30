b5e62c98b391e3a360cd8c302add2df795c81b39
garner1 <silvano.garnerone@gmail.com>
2018-06-30 10:41:21 +0200

    modification to merge bamfiles

--- bin/module/merge_bamfiles_dedup.sh
+++ bin/module/merge_bamfiles_dedup.sh
@@ -1,17 +1,13 @@
 #!/usr/bin/env bash
 
-parallel "samtools merge merged_XZ{}.bam XZ{}_BICRO65/outdata/*.deduplicated.bam XZ{}/outdata/*.deduplicated.bam" ::: 74 75 76 77 78
+bam1=$1
+bam2=$2
+newdir=$3
 
-# parallel "samtools sort merged_XZ{}.bam -o temporary_{}.bam" ::: 74 75 76 77 78
-# parallel "samtools index temporary_{}.bam" ::: 74 75 76 77 78
-
-# parallel --memfree 50G "umi_tools dedup -I temporary_{}.bam -S {}_merged.deduplicated.bam --edit-distance-threshold 2 -L {}_group.log" ::: 74 75 76 77 78
-
-# parallel "bedtools bamtobed -i {}_merged.deduplicated.2.bam > XZ{}_merged.bed" ::: 74 75 76 77 78
-
-# file=$1
-# echo 'File name:'$file
-# echo '#################################'
-# echo '#UMI with a ref-cutsite:' `samtools view "$file" | wc -l`
-# echo '#unique sites:' `samtools view "$file" |  cut -f3,4 | LC_ALL=C sort -u | wc -l`
-# echo '#################################'
+mkdir $newdir
+samtools merge $newdir/merged.bam $bam1 $bam2
+cd $newdir
+samtools sort merged.bam -o merged.sorted.bam
+samtools index merged.sorted.bam
+umi_tools dedup -I merged.sorted.bam -S merged.deduplicated.bam --edit-distance-threshold 2 -L merged.group.log
+bedtools bamtobed -i merged.deduplicated.bam > merged.deduplicated.bed

21ea1b47639a3c013d6eb8eaa2074aa766841341
garner1 <silvano.garnerone@gmail.com>
2019-01-25 14:13:22 +0100

    minor

--- bin/module/parallel_scan.sh
+++ bin/module/parallel_scan.sh
@@ -25,19 +25,19 @@ if [ "$mode" == "SE" ];then
     fi
 fi
 if [ "$mode" == "PE" ];then
-    # if [ ! -f "$in"/processed.2.fastq.gz ]; then
-	# umi_tools extract --stdin="$r1" --read2-in="$r2" --bc-pattern=NNNNNNNNXXXXXXXX --log=processed.log --stdout "$in"/processed.1.fastq.gz --read2-out="$in"/processed.2.fastq.gz
-	# gunzip -c "$in"/processed.1.fastq.gz > "$in"/r1.fq & pid1=$!
-	# gunzip -c "$in"/processed.2.fastq.gz > "$in"/r2.fq & pid2=$!
-	# wait $pid1
-	# wait $pid2
+    if [ ! -f "$in"/processed.2.fastq.gz ]; then
+	umi_tools extract --stdin="$r1" --read2-in="$r2" --bc-pattern=NNNNNNNNXXXXXXXX --log=processed.log --stdout "$in"/processed.1.fastq.gz --read2-out="$in"/processed.2.fastq.gz
+	gunzip -c "$in"/processed.1.fastq.gz > "$in"/r1.fq & pid1=$!
+	gunzip -c "$in"/processed.2.fastq.gz > "$in"/r2.fq & pid2=$!
+	wait $pid1
+	wait $pid2
 	cat $in/r1.fq | cut -d':' -f-7 | paste - - - - | LC_ALL=C sort --parallel=8 --temporary-directory=$HOME/tmp -k1,1 > $in/r1oneline.fq & pid1=$!
 	cat $in/r1.fq | cut -d':' -f-7 | paste - - - - | cut -f 1,2 | sed 's/^@/>/' | tr "\t" "\n" > $in/r1.fa & pid2=$!
 	cat $in/r2.fq | cut -d':' -f-7 | paste - - - - | LC_ALL=C sort --parallel=8 --temporary-directory=$HOME/tmp -k1,1 > $in/r2oneline.fq & pid3=$!
 	wait $pid1
 	wait $pid2
 	wait $pid3
-    # fi
+    fi
 fi
 
 ################################################################################

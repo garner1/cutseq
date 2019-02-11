d125df6dd4ea929624a4696db9f32b593a031908
garner1 <silvano.garnerone@gmail.com>
2019-02-11 14:01:03 +0100

    minor

--- bin/main.sh
+++ bin/main.sh
@@ -19,7 +19,8 @@ datadir=$HOME/Work/dataset/cutseq && mkdir -p $datadir/$experiment
 in=$datadir/$experiment/indata && mkdir -p $in
 out=$datadir/$experiment/outdata && mkdir -p $out
 aux=$datadir/$experiment/auxdata && mkdir -p $aux
-refgen=~/Work/genomes/Homo_sapiens.GRCh37.dna.primary_assembly.fa/GRCh37.fa # full path to reference genome
+#refgen=~/Work/genomes/Homo_sapiens.GRCh37.dna.primary_assembly.fa/GRCh37.fa # full path to reference genome
+refgen=/home/garner1/Work/dataset/agilent/S07604715_Padded.woChr.fa
 
 echo
 echo Processing $experiment

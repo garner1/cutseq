e357707b0a7dfa55da5b5185b713d05ce525c74c
garner1 <silvano.garnerone@gmail.com>
2019-03-18 10:16:51 +0100

    minor

---
+++ bin/runs/run_pipeline_BICRO147.sh
@@ -0,0 +1,22 @@
+#!/bin/usr/env bash
+
+# bash main.sh XZ138BICRO147 human SE /home/garner1/Dropbox/pipelines/cutseq/pattern/CATCATCC-CATG_XZ138BICRO147 CATG /home/garner1/Work/dataset/BC147/fastq/XZ138.fastq.gz
+
+# bash main.sh XZ139BICRO147 human SE /home/garner1/Dropbox/pipelines/cutseq/pattern/GTCGTTCC-CATG_XZ139BICRO147 CATG /home/garner1/Work/dataset/BC147/fastq/XZ139.fastq.gz
+
+# bash main.sh XZ140BICRO147 human SE /home/garner1/Dropbox/pipelines/cutseq/pattern/ACGACTCC-CATG_XZ140BICRO147 CATG /home/garner1/Work/dataset/BC147/fastq/XZ140.fastq.gz
+
+# bash main.sh XZ141BICRO147 human SE /home/garner1/Dropbox/pipelines/cutseq/pattern/TGATGTCC-CATG_XZ141BICRO147 CATG /home/garner1/Work/dataset/BC147/fastq/XZ141.fastq.gz
+
+# bash main.sh XZ142BICRO147 human SE /home/garner1/Dropbox/pipelines/cutseq/pattern/CATCAGAA-CATG_XZ142BICRO147 CATG /home/garner1/Work/dataset/BC147/fastq/XZ142.fastq.gz
+
+bash main.sh XZ143BICRO147 human SE /home/garner1/Dropbox/pipelines/cutseq/pattern/GTCGTGAA-CATG_XZ143BICRO147 CATG /home/garner1/Work/dataset/BC147/fastq/XZ143.fastq.gz
+
+bash main.sh XZ143BICRO147 human SE /home/garner1/Dropbox/pipelines/cutseq/pattern/ACGACGAA-CATG_XZ143BICRO147 CATG /home/garner1/Work/dataset/BC147/fastq/XZ143.fastq.gz
+
+bash main.sh XZ143BICRO147 human SE /home/garner1/Dropbox/pipelines/cutseq/pattern/TGATGGAA-CATG_XZ143BICRO147 CATG /home/garner1/Work/dataset/BC147/fastq/XZ143.fastq.gz
+
+bash main.sh XZ143BICRO147 human SE /home/garner1/Dropbox/pipelines/cutseq/pattern/TCACACGC-CATG_XZ143BICRO147 CATG /home/garner1/Work/dataset/BC147/fastq/XZ143.fastq.gz
+
+bash main.sh XZ143BICRO147 human SE /home/garner1/Dropbox/pipelines/cutseq/pattern/CGTGTCGC-CATG_XZ143BICRO147 CATG /home/garner1/Work/dataset/BC147/fastq/XZ143.fastq.gz
+

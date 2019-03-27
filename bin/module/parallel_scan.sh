6e299b81986233a1b0baf8c1d54bb8e707d0e28e
garner1 <silvano.garnerone@gmail.com>
2019-03-27 13:52:45 +0100

    better parallelization

--- bin/module/parallel_scan.sh
+++ bin/module/parallel_scan.sh
@@ -10,13 +10,13 @@ r1=$5
 r2=$6
 
 echo "Process the fastq file ..."
-
 ################################################################################
 echo "Unzip the raw data file ..."
-
 if [ "$mode" == "SE" ];then
-    if [ ! -f "$in"/processed.fastq.gz ]; then
+    if [ ! -f "$in"/r1_0.fq.gz ]; then
 	zcat "$r1" | cut -d':' -f-7 | gzip - > $in/r1_0.fq.gz
+    fi
+    if [ ! -f "$in"/processed.fastq.gz ]; then
 	umi_tools extract --stdin="$in/r1_0.fq.gz" --bc-pattern=NNNNNNNNXXXXXXXX --log=processed.log --stdout "$in"/processed.fastq.gz
 	gunzip -c "$in"/processed.fastq.gz > "$in"/r1.fq
 	cat $in/r1.fq | paste - - - - | LC_ALL=C sort --parallel=8 --temporary-directory=$HOME/tmp -k1,1 > $in/r1oneline.fq & pid1=$!
@@ -26,9 +26,13 @@ if [ "$mode" == "SE" ];then
     fi
 fi
 if [ "$mode" == "PE" ];then
-    if [ ! -f "$in"/processed.2.fastq.gz ]; then
+    if [ ! -f "$in"/r1_0.fq.gz ]; then
 	zcat "$r1" | cut -d':' -f-7 | gzip > $in/r1_0.fq.gz
+    fi
+    if [ ! -f "$in"/r2_0.fq.gz ]; then
 	zcat "$r2" | cut -d':' -f-7 | gzip > $in/r2_0.fq.gz
+    fi
+    if [ ! -f "$in"/processed.2.fastq.gz ]; then
 	umi_tools extract --stdin="$in/r1_0.fq.gz" --read2-in="$in/r2_0.fq.gz" --bc-pattern=NNNNNNNNXXXXXXXX --log=processed.log --stdout "$in"/processed.1.fastq.gz --read2-out="$in"/processed.2.fastq.gz
 	gunzip -c "$in"/processed.1.fastq.gz > "$in"/r1.fq & pid1=$!
 	gunzip -c "$in"/processed.2.fastq.gz > "$in"/r2.fq & pid2=$!
@@ -42,14 +46,13 @@ if [ "$mode" == "PE" ];then
 	wait $pid3
     fi
 fi
-
 ################################################################################
 echo "Generate patfiles for each barcode ..."
 len=`echo $cutsite|awk '{print length}'`
 olddir=`echo $PWD`
 cd $in
 cat $barcode_file |
-    awk -v len="$len" '{print "^ ",substr($1,1,8)"[2,1,1]",substr($1,9,len)"[2,1,1]","1...1000","$" > "barcode_"substr($1,1,8)}'
+    awk -v len="$len" '{print "^ ",substr($1,1,8)"[0,0,0]",substr($1,9,len)"[2,1,1]","1...1000","$" > "barcode_"substr($1,1,8)}'
 cd $olddir
 ################################################################################
 echo "Split FA files to satisfy scan_for_match 100M lines limit ..."

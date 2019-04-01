017c21f19624680bda3cbd3dddd5dc0910b3203d
garner1 <silvano.garnerone@gmail.com>
2019-04-01 16:47:21 +0200

    bicro157

--- bin/runs/run_pipeline_BICRO157.sh
+++ bin/runs/run_pipeline_BICRO157.sh
@@ -1,8 +1,4 @@
 #!/bin/usr/env bash
 
-bash main.sh XZ176BICRO157 human SE /home/garner1/Dropbox/pipelines/cutseq/pattern/XZ176BICRO157 AAGCTT /home/garner1/Work/dataset/fastq/XZ176.fastq.gz
-
-bash main.sh XZ177BICRO157 human SE /home/garner1/Dropbox/pipelines/cutseq/pattern/XZ177BICRO157 AAGCTT /home/garner1/Work/dataset/fastq/XZ177.fastq.gz
-
 bash main.sh XZ178BICRO157 human SE /home/garner1/Dropbox/pipelines/cutseq/pattern/XZ178BICRO157 AAGCTT /home/garner1/Work/dataset/fastq/XZ178.fastq.gz
 

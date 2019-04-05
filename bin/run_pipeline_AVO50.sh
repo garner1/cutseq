061cc5a1b7376dd09c857d00ff720f13e1d25e85
garner1 <silvano.garnerone@gmail.com>
2019-04-05 13:42:42 +0200

    NC samples final

--- bin/run_pipeline_AVO50.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/usr/env bash
-
-if [ -f /home/garner1/Work/dataset/fastq/NC88.fastq.gz ]; then
-bash main.sh NC88AVO50 human SE /home/garner1/Dropbox/pipelines/cutseq/pattern/NC88AVO50 AAGCTT /home/garner1/Work/dataset/fastq/NC88.fastq.gz
-fi
--- bin/run_pipeline_AVO73.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/usr/env bash
-
-bash main.sh NC105AVO73 human SE /home/garner1/Dropbox/pipelines/cutseq/pattern/NC105AVO73 AAGCTT /home/garner1/Work/dataset/fastq/NC105.fastq.gz
-

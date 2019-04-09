953e3ef538032c427f80b08c5fab2d1f95e03e85
garner1 <silvano.garnerone@gmail.com>
2019-04-09 09:12:09 +0200

    XZ178BC157+160 done

--- bin/run_pipeline_BICRO160.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/usr/env bash
-
-if [ -f /home/garner1/Work/dataset/fastq/BC157+160XZ178/XZ178.fastq.gz ]; then
-bash main.sh XZ178BICRO160 human SE /home/garner1/Dropbox/pipelines/cutseq/pattern/XZ178BICRO160 AAGCTT /home/garner1/Work/dataset/fastq/BC157+160XZ178/XZ178.fastq.gz
-fi

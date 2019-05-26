7ace6cc8c78e4ea0fcac51c26b41dada3656564d
garner1 <silvano.garnerone@gmail.com>
2019-05-26 08:04:42 +0200

    XZ166 done

--- bin/runs/run_pipeline_XZ166BC155.sh
+++ bin/runs/run_pipeline_XZ166BC155.sh
@@ -1,5 +1,5 @@
 #!/bin/usr/env bash
 
-if [ -f /home/garner1/Work/dataset/fastq/BC155/XZ166.fastq.gz ]; then
-bash main.sh XZ166BC155 human SE /home/garner1/Dropbox/pipelines/cutseq/pattern/XZ166BC155 AAGCTT /home/garner1/Work/dataset/fastq/BC155/XZ166.fastq.gz
+if [ -f /home/garner1/Work/dataset/fastq/XZ166.fastq.gz ]; then
+bash main.sh XZ166BC155 human SE /home/garner1/Dropbox/pipelines/cutseq/pattern/XZ166BC155 AAGCTT /home/garner1/Work/dataset/fastq/XZ166.fastq.gz
 fi

75d76116c73940f54718d0f88a963f9f57dbe04d
garner1 <silvano.garnerone@gmail.com>
2019-05-28 13:11:59 +0200

    BICRO168 done

---
+++ bin/runs/run_pipeline_XZ184BC168.sh
@@ -0,0 +1,5 @@
+#!/bin/usr/env bash
+
+if [ -f /home/garner1/Work/dataset/fastq/XZ184.fastq.gz ]; then
+bash main.sh XZ184BC168 human SE /home/garner1/Dropbox/pipelines/cutseq/pattern/XZ184BC168 CATG /home/garner1/Work/dataset/fastq/XZ184.fastq.gz
+fi

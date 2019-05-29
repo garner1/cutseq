c4db73e206288809fdf75db6d10fd678d0016ea4
garner1 <silvano.garnerone@gmail.com>
2019-05-29 10:09:02 +0200

    alfred on deduplicated final bam file

---
+++ bin/run_pipeline_XZ183BC168.sh
@@ -0,0 +1,5 @@
+#!/bin/usr/env bash
+
+if [ -f /home/garner1/Work/dataset/fastq/XZ183.fastq.gz ]; then
+bash main.sh XZ183BC168 human SE /home/garner1/Dropbox/pipelines/cutseq/pattern/XZ183BC168 CATG /home/garner1/Work/dataset/fastq/XZ183.fastq.gz
+fi
---
+++ bin/run_pipeline_XZ202BC168.sh
@@ -0,0 +1,5 @@
+#!/bin/usr/env bash
+
+if [ -f /home/garner1/Work/dataset/fastq/XZ202.fastq.gz ]; then
+bash main.sh XZ202BC168 human SE /home/garner1/Dropbox/pipelines/cutseq/pattern/XZ202BC168 CATG /home/garner1/Work/dataset/fastq/XZ202.fastq.gz
+fi

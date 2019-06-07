67cb47c10183aef36d2a6f4ab93441d390071deb
garner1 <silvano.garnerone@gmail.com>
2019-06-07 13:42:01 +0200

    minor

--- manuscript_scripts/split_amp-del-null.sh
+++ manuscript_scripts/split_amp-del-null.sh
@@ -7,3 +7,4 @@ tneg=-0.4150375 #log2(1.5/2)
 tail -n+2 $file | awk -v tpos=$tpos '$5>tpos' | cut -f1 | tr ':-' '\t\t' | bedtools merge -i stdin | sort -k1,1 -k2,2n > ${file}.amp.bed
 tail -n+2 $file | awk -v tneg=$tneg '$5<tneg' | cut -f1 | tr ':-' '\t\t' | bedtools merge -i stdin | sort -k1,1 -k2,2n > ${file}.del.bed
 tail -n+2 $file | awk -v tpos=$tpos '$5<tpos' | awk -v tneg=$tneg '$5>tneg' | cut -f1 | tr ':-' '\t\t' | bedtools merge -i stdin | sort -k1,1 -k2,2n > ${file}.null.bed
+

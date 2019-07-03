8ade278434294a0200a93b3fff51a473c0d6bdfa
garner1 <silvano.garnerone@gmail.com>
2019-07-03 14:27:56 +0200

    Start processing NC25AVO29

--- bin/run_pipeline_NC25AVO29.sh
+++ bin/run_pipeline_NC25AVO29.sh
@@ -1,9 +1,7 @@
 #!/bin/usr/env bash
 
-if [ -f /home/garner1/Work/dataset/fastq/NC25AVO29_R1.fastq.gz ]; then
-if [ -f /home/garner1/Work/dataset/fastq/NC25AVO29_R2.fastq.gz ]; then
+if [ -f /home/garner1/Work/dataset/fastq/NC25AVO29.fastq.gz ]; then
 if [ -f /home/garner1/Work/pipelines/cutseq/pattern/NC25AVO29 ]; then
-bash main.sh NC25AVO29 human PE /home/garner1/Work/pipelines/cutseq/pattern/NC25AVO29 AAGCTT /home/garner1/Work/dataset/fastq/NC25AVO29_R1.fastq.gz /home/garner1/Work/dataset/fastq/NC25AVO29_R2.fastq.gz
-fi
+bash main.sh NC25AVO29 human SE /home/garner1/Work/pipelines/cutseq/pattern/NC25AVO29 AAGCTT /home/garner1/Work/dataset/fastq/NC25AVO29.fastq.gz
 fi
 fi

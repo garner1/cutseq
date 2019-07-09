b9e39c85081a9a92986ed401e2bf2581451d3c2d
garner1 <silvano.garnerone@gmail.com>
2019-07-09 14:32:50 +0200

    Done with processing test

--- bin/main.sh
+++ bin/main.sh
@@ -48,12 +48,12 @@ do
     	samtools view -h -q $quality ${aux}/${barcode}.all.bam | samtools sort -@ 8 -T ${aux}/${barcode} > ${aux}/${barcode}.q${quality}.bam 
     	parallel "samtools index -@ 8 {}" ::: ${aux}/${barcode}.all.bam ${aux}/${barcode}.q${quality}.bam
     	if [ ${mode} == "SE" ];	then
-	    /usr/local/share/anaconda3/bin/umi_tools group -I ${aux}/${barcode}.q${quality}.bam --group-out=${aux}/${barcode}.q${quality}.grouped.tsv --log=${aux}/${barcode}.grouped.log 
-    	    /usr/local/share/anaconda3/bin/umi_tools dedup -I ${aux}/${barcode}.q${quality}.bam -S ${out}/${barcode}.deduplicated.bam -L ${out}/${barcode}.group.log 
+	    /usr/local/share/anaconda3/bin/umi_tools group -I ${aux}/${barcode}.q${quality}.bam --group-out=${aux}/${barcode}.q${quality}.grouped.tsv --log=${aux}/${barcode}.grouped.log --mapping-quality=30
+    	    /usr/local/share/anaconda3/bin/umi_tools dedup -I ${aux}/${barcode}.q${quality}.bam -S ${out}/${barcode}.deduplicated.bam -L ${out}/${barcode}.group.log --mapping-quality=30
     	fi
     	if [ ${mode} == "PE" ];	then
-	    /usr/local/share/anaconda3/bin/umi_tools group -I ${aux}/${barcode}.q${quality}.bam --group-out=${aux}/${barcode}.q${quality}.grouped.tsv --log=${aux}/${barcode}.grouped.log --paired
-    	    /usr/local/share/anaconda3/bin/umi_tools dedup -I ${aux}/${barcode}.q${quality}.bam --paired -S ${out}/${barcode}.deduplicated.bam -L ${out}/${barcode}.group.log 
+	    /usr/local/share/anaconda3/bin/umi_tools group -I ${aux}/${barcode}.q${quality}.bam --group-out=${aux}/${barcode}.q${quality}.grouped.tsv --log=${aux}/${barcode}.grouped.log --paired --mapping-quality=30
+    	    /usr/local/share/anaconda3/bin/umi_tools dedup -I ${aux}/${barcode}.q${quality}.bam --paired -S ${out}/${barcode}.deduplicated.bam -L ${out}/${barcode}.group.log --mapping-quality=30
     	fi
     	samtools sort -@ 8 ${out}/${barcode}.deduplicated.bam -o ${out}/${barcode}.deduplicated.q${quality}.bam && rm -f ${out}/${barcode}.deduplicated.bam
     	# parallel "/usr/local/share/anaconda3/bin/alfred qc -r /home/garner1/Work/genomes/Homo_sapiens.GRCh37.dna.primary_assembly.fa/GRCh37.fa \

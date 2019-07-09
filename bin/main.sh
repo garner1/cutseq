22ec9b0aa50d0c9c9efc3693bef562f1f70d6ca4
garner1 <silvano.garnerone@gmail.com>
2019-07-09 14:41:26 +0200

    Done with processing test

--- bin/main.sh
+++ bin/main.sh
@@ -45,7 +45,6 @@ do
 
     count=$(samtools view -c ${aux}/${barcode}.all.bam)
     if [ $count -ne 0 ]; then 
-    	# samtools view -h -q $quality ${aux}/${barcode}.all.bam | samtools sort -@ 8 -T ${aux}/${barcode} > ${aux}/${barcode}.q${quality}.bam 
     	samtools index -@ 8 ${aux}/${barcode}.all.bam
     	if [ ${mode} == "SE" ];	then
 	    /usr/local/share/anaconda3/bin/umi_tools group -I ${aux}/${barcode}.all.bam --group-out=${aux}/${barcode}.q${quality}.grouped.tsv --log=${aux}/${barcode}.grouped.log --mapping-quality=${quality}
@@ -56,10 +55,10 @@ do
     	    /usr/local/share/anaconda3/bin/umi_tools dedup -I ${aux}/${barcode}.all.bam --paired -S ${out}/${barcode}.deduplicated.bam -L ${out}/${barcode}.group.log --mapping-quality=${quality}
     	fi
     	samtools sort -@ 8 ${out}/${barcode}.deduplicated.bam -o ${out}/${barcode}.deduplicated.q${quality}.bam && rm -f ${out}/${barcode}.deduplicated.bam
-    	# parallel "/usr/local/share/anaconda3/bin/alfred qc -r /home/garner1/Work/genomes/Homo_sapiens.GRCh37.dna.primary_assembly.fa/GRCh37.fa \
-	# 				      -b /home/garner1/Work/dataset/agilent/S07604715_Covered.woChr.bed \
-	# 				      -j {.}.json.gz -o {.}.tsv.gz {}" \
-	# 				      ::: ${out}/${barcode}.deduplicated.q${quality}.bam ${aux}/${barcode}.all.bam 
+    	parallel "/usr/local/share/anaconda3/bin/alfred qc -r /home/garner1/Work/genomes/Homo_sapiens.GRCh37.dna.primary_assembly.fa/GRCh37.fa \
+					      -b /home/garner1/Work/dataset/agilent/S07604715_Covered.woChr.bed \
+					      -j {.}.json.gz -o {.}.tsv.gz {}" \
+					      ::: ${out}/${barcode}.deduplicated.q${quality}.bam ${aux}/${barcode}.all.bam 
 	rm -f processed.log
     	# echo "Conversion to bed file ..."
     	# bam2bed < ${out}/${barcode}.deduplicated.bam | cut -f-17 > ${out}/${barcode}.deduplicated.bed # convert using bedops bam2bed

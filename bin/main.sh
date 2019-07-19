50afd05e0b829d26b226af469c53c50e7c04f4e9
garner1 <silvano.garnerone@gmail.com>
2019-07-19 13:13:59 +0200

    Done with processing test

--- bin/main.sh
+++ bin/main.sh
@@ -33,39 +33,38 @@ do
     i=`expr $i + 1`
     if [ ${mode} == "PE" ]
     then
-    	bwa mem -v 1 -t $numbproc $refgen ${indir}/barcode_${barcode}.fq ${indir}/barcode_${barcode}-r2.fq | samtools sort -@ 8 -T ${aux}/${experiment} > ${aux}/${barcode}.all.bam
+    	bwa mem -v 1 -t $numbproc $refgen ${indir}/barcode_${barcode}.fq ${indir}/barcode_${barcode}-r2.fq | samtools sort -@ 8 -T ${aux}/${experiment} > ${out}/${barcode}.all.bam
     fi
     if [ ${mode} == "SE" ]
     then
-    	bwa mem -v 1 -t $numbproc $refgen ${indir}/barcode_${barcode}.fq | samtools sort -@ 8 -T ${aux}/${experiment} > ${aux}/${barcode}.all.bam
+    	bwa mem -v 1 -t $numbproc $refgen ${indir}/barcode_${barcode}.fq | samtools sort -@ 8 -T ${aux}/${experiment} > ${out}/${barcode}.all.bam
     fi
-
-    count=$(samtools view -c ${aux}/${barcode}.all.bam)
+    count=$(samtools view -c ${out}/${barcode}.all.bam)
     if [ $count -ne 0 ]; then 
-    	samtools index -@ 8 ${aux}/${barcode}.all.bam
+    	samtools index -@ 8 ${out}/${barcode}.all.bam
     	if [ ${mode} == "SE" ];	then
 	    /usr/local/share/anaconda3/bin/umi_tools group \
-						     -I ${aux}/${barcode}.all.bam --group-out=${aux}/${barcode}.q${quality}.grouped.tsv \
-						     --log=${aux}/${barcode}.grouped.log --mapping-quality=${quality} & pid1=$!
+						     -I ${out}/${barcode}.all.bam --group-out=${out}/${barcode}.q${quality}.grouped.tsv \
+						     --log=${out}/${barcode}.grouped.log --mapping-quality=${quality} & pid1=$!
     	    /usr/local/share/anaconda3/bin/umi_tools dedup \
-						     -I ${aux}/${barcode}.all.bam -S ${out}/${barcode}.deduplicated.bam -L ${out}/${barcode}.group.log --mapping-quality=${quality} & pid2=$!
+						     -I ${out}/${barcode}.all.bam -S ${out}/${barcode}.deduplicated.bam -L ${out}/${barcode}.group.log --mapping-quality=${quality} & pid2=$!
     	fi
     	if [ ${mode} == "PE" ];	then
 	    /usr/local/share/anaconda3/bin/umi_tools group \
-						     -I ${aux}/${barcode}.all.bam --paired --group-out=${aux}/${barcode}.q${quality}.grouped.tsv \
-						     --log=${aux}/${barcode}.grouped.log --mapping-quality=${quality} & pid1=$!
+						     -I ${out}/${barcode}.all.bam --paired --group-out=${out}/${barcode}.q${quality}.grouped.tsv \
+						     --log=${out}/${barcode}.grouped.log --mapping-quality=${quality} & pid1=$!
     	    /usr/local/share/anaconda3/bin/umi_tools dedup \
-						     -I ${aux}/${barcode}.all.bam --paired -S ${out}/${barcode}.deduplicated.bam -L ${out}/${barcode}.group.log --mapping-quality=${quality} & pid2=$!
+						     -I ${out}/${barcode}.all.bam --paired -S ${out}/${barcode}.deduplicated.bam -L ${out}/${barcode}.group.log --mapping-quality=${quality} & pid2=$!
     	fi
 	wait $pid1
 	wait $pid2
-	gzip ${aux}/${barcode}.q${quality}.grouped.tsv & pid1=$!
+	gzip ${out}/${barcode}.q${quality}.grouped.tsv & pid1=$!
     	samtools sort -@ 8 ${out}/${barcode}.deduplicated.bam -o ${out}/${barcode}.deduplicated.q${quality}.bam && rm -f ${out}/${barcode}.deduplicated.bam & pid2=$!
 	wait $pid2
     	parallel "/usr/local/share/anaconda3/bin/alfred qc -r /home/garner1/Work/genomes/Homo_sapiens.GRCh37.dna.primary_assembly.fa/GRCh37.fa \
 					      -b /home/garner1/Work/dataset/agilent/S07604715_Covered.woChr.bed \
 					      -j {.}.json.gz {}" \
-		 ::: ${out}/${barcode}.deduplicated.q${quality}.bam ${aux}/${barcode}.all.bam
+		 ::: ${out}/${barcode}.deduplicated.q${quality}.bam ${out}/${barcode}.all.bam
 	wait $pid1
 	rm -f processed.log
     fi

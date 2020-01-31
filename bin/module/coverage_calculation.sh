c5813d710db2f7a968e8e8648b113eb0b8bb0320
garner1 <silvano.garnerone@gmail.com>
2020-01-31 13:18:47 +0100

    cleaning

--- bin/module/coverage_calculation.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-bamfile=$1
-
-bedtools genomecov -ibam $bamfile -bga > $bamfile.coverage.tsv
--- bin/module/mappability.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-exp=$1
-
-# need to parallelize wrt to patfile as well
-parallel -k "scan_for_matches patfile < {} >> $PWD/hits" ::: ~/Work/silicoDigest/F{1..24}
-
